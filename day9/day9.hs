7a73a65ddfdb1e560d00482f8cc22d5d4d1ea8a1
Mark Gritter <mgritter@gmail.com>
2018-12-31 14:46:43 -0800

    Shifted to lists only, still too slow for part 2.

--- day9/day9.hs
+++ day9/day9.hs
@@ -1,64 +1,41 @@
 #!/usr/bin/env stack
 -- stack --install-ghc runghc --package array-0.5.3.0
 
---import Data.Array.IArray (Array,array,(!),ixmap,(//),bounds,elems)
-import Data.Array.Unboxed (UArray,array,(!),ixmap,(//),bounds,elems)
 import qualified Data.Map.Strict as Map
 import Data.List (intercalate,scanl,(!!),foldl',maximum)
 
-start :: Circle
-start = array (0,0) [(0,0)]
---numPlayers = 470
---lastMarble = 72170
-
 type Marble = Int
-type Circle = UArray Int Int
+type Circle = [Int]
+
+start :: Circle
+start = [0]
 
 data Round =
   Round {
    player :: Int,
-   current :: Int,
+   -- Current position will always be location 0 in the circle
    circle :: Circle,
    score :: Map.Map Int Int
   } deriving (Show)
 
--- Rewrite the indices so that 0..i <- 0..i and
--- (i+2)..(n+1) <- (i+1)..n
--- (i+1) needs to come from somewhere, we'll just duplicate i.
-rewriteIndexInsert :: Int -> Int -> Int
-rewriteIndexInsert i x = if x <= i then x else x-1
+-- start with [current 1 2 ...]
+-- insert the marble [current 1 marble 2 ...]
+-- make current the nely inserted [marble 2 ...] ++ [current 1]
+insertMarble :: Circle -> Marble -> Circle
+insertMarble (c:c1:ms) m =
+  (m:ms) ++ [c, c1]
+insertMarble (c:[]) m = [m, c]
 
--- Rewrite the indices so that 0..d-1 <- 0..d-1 and
--- d..(n-1) <- (d+1)..n
-rewriteIndexDelete :: Int -> Int -> Int
-rewriteIndexDelete d x = if x < d then x else x+1
-
-moduloSize :: Int -> Circle -> Int
-moduloSize x a =
-  let n = ((snd (bounds a)) + 1) in
-    ( x + n ) `mod` n
-  
-increaseBounds :: Circle -> (Int,Int)
-increaseBounds a =
-  let b = bounds a in
-    (fst b, 1 + snd b)
-
-decreaseBounds :: Circle -> (Int,Int)
-decreaseBounds a =
-  let b = bounds a in
-    (fst b, (snd b) - 1)
-  
-insertMarble :: Circle -> Int -> Marble -> (Circle, Int)
-insertMarble a current m =
-  let after = (current + 1) `moduloSize` a in
-    ( (ixmap (increaseBounds a) (rewriteIndexInsert after) a) //
-      [(after+1,m)],
-      after+1 )
-
-removeMarble :: Circle -> Int -> Circle
-removeMarble a pos =
-  ixmap (decreaseBounds a) (rewriteIndexDelete pos) a
-
+-- start with [current ... m8 m7 m6 m5 m4 m3 m2 m1]
+-- remove m7 and make m6 current
+-- [m6 .. m1] ++ [current ... m8] 
+removeMarble :: Circle -> (Circle, Marble)
+removeMarble a =
+  let len = length a
+      parts = splitAt (len - 7) a in
+    ( (tail (snd parts)) ++ (fst parts),
+      (head (snd parts)) )
+          
 nextPlayer :: Int -> Int -> Int
 nextPlayer numPlayers x = if x == numPlayers then 1 else x+1
 
@@ -67,27 +44,27 @@ increaseScore x (Just y) = Just (x+y)
 increaseScore x Nothing = Just x
 
 nextRound :: Int -> Round -> Marble -> Round
-nextRound numPlayers (Round p current circle score) m
+nextRound numPlayers (Round p circle score) m
   | m `mod` 23 == 0 =
-    let remove = ( current - 7 ) `moduloSize` circle
-        nextCurrent = remove
-        points = (circle ! remove) + m
-        nextScore = Map.alter (increaseScore points) p score
-        nextCirc = removeMarble circle remove in
-      Round (nextPlayer numPlayers p) nextCurrent nextCirc nextScore      
+    let result = removeMarble circle
+        removed = snd result
+        circle' = fst result        
+        points = removed + m
+        score' = Map.alter (increaseScore points) p score in
+      Round (nextPlayer numPlayers p) circle' score'
   | otherwise =
-    let nextCirc = insertMarble circle current m in
-      Round (nextPlayer numPlayers p) (snd nextCirc) (fst nextCirc) score
+    let circle' = insertMarble circle m in
+      Round (nextPlayer numPlayers p) circle' score
       
 main :: IO ()
 
 showCircle :: Circle -> String
-showCircle c = intercalate " " (map show (elems c))
+showCircle c = intercalate " " (map show c)
 
 showGame :: Round -> String
-showGame (Round p current circle score) = "[" ++ (show p) ++ "] {" ++ (show current) ++ "} " ++ (showCircle circle)
+showGame (Round p circle score) = "[" ++ (show p) ++ "] " ++ (showCircle circle)
 
-round0 = Round 1 0 start Map.empty
+round0 = Round 1 start Map.empty
 
 playRounds :: Int -> Int -> Round
 playRounds numPlayers lastMarble = foldl' (nextRound numPlayers) round0 [1..lastMarble]
